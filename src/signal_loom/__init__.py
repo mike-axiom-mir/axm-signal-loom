b2dbcc82030f54c96f374a201fef59c66138c529
mike-axiom-mir <miketobi90@gmail.com>
2026-09-20 04:33:19 +0200

    feat: expose Signal Machine v0.5 API

--- src/signal_loom/__init__.py
+++ src/signal_loom/__init__.py
@@ -1,18 +1,39 @@
 from .core import SignalLoom, weave, weave_capture
 from .internet_eye import InternetEyeInfluence, ShodanInfluenceAdapter
+from .machine import (
+    CaptureRecipe,
+    CaptureSession,
+    SignalMachine,
+    SourceInput,
+    SourceOrganSpec,
+    SourceRegistry,
+)
 from .magnet import SignalMagnet
-from .model import CreativityPacket, Signal, SourceCapture, SourcePacket
+from .model import (
+    CreativityPacket,
+    Signal,
+    SourceCapture,
+    SourceContribution,
+    SourcePacket,
+)
 
 __all__ = [
     "Signal",
     "SourcePacket",
+    "SourceContribution",
     "SourceCapture",
     "CreativityPacket",
+    "SourceOrganSpec",
+    "SourceRegistry",
+    "SourceInput",
+    "CaptureRecipe",
+    "CaptureSession",
     "SignalMagnet",
+    "SignalMachine",
     "InternetEyeInfluence",
     "ShodanInfluenceAdapter",
     "SignalLoom",
     "weave",
     "weave_capture",
 ]
-__version__ = "0.4.0"
+__version__ = "0.5.0"
